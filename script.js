e7cdb917d741d93b211c338602f2815298fb0da6
Samoht07 <341737@talnet.nl>
2020-02-07 14:50:06 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+function bgKleur() {
+    var element = document.getElementById("body");
+    element.style.backgroundColor = document.getElementById("bgKleur").value;
+}
+
+function txtKleur() {
+    var texten = document.getElementById("hoofd");
+    texten.style.color = document.getElementById("txtKleur").value;
+}
